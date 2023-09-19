fbc261f50984e93ec73b418e4242baa5ac8a98b4
Maximiliano Biondo <mbiondo@hcdn.gob.ar>
2023-09-19 15:13:19 -0300

    initial commit

---
+++ types/index.ts
@@ -0,0 +1,28 @@
+export interface Material {
+  name: string;
+  url: string;
+  description: string;
+  advantages: string[];
+  disadvantage: string[];
+}
+
+export interface Project {
+  serviceId: number;
+  service?: Service;
+  slug: string;
+  title: string;
+  description: string;
+  images: Image[];
+}
+
+export interface Image {
+  title: string;
+  url: string;
+}
+
+export interface Service {
+  name: string;
+  icon: string;
+  id: number;
+  description: string;
+}
