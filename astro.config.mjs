de32418ee7ce4457f17dc4caa0f32935b5235026
Maximiliano Biondo <mbiondo@hcdn.gob.ar>
2023-09-21 11:49:22 -0300

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import tailwind from '@astrojs/tailwind';
 import robotsTxt from 'astro-robots-txt';
 import vercel from '@astrojs/vercel/serverless';
 
+import sitemap from "astro-sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lumalux.com.ar',
-  integrations: [tailwind(), robotsTxt()],
+  integrations: [tailwind(), robotsTxt(), sitemap()],
   output: 'server',
   adapter: vercel()
 });
