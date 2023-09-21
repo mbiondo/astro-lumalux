acb2d7d466463cba7d7810355fc64a9e05081cc9
Maximiliano Biondo <mbiondo@hcdn.gob.ar>
2023-09-21 11:18:53 -0300

    Add robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
+import robotsTxt from 'astro-robots-txt';
 import vercel from '@astrojs/vercel/serverless';
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://lumalux.com.ar',
+  integrations: [tailwind(), robotsTxt()],
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel()
 });
