aa665d8435203af8ae374fb8b9cd4c53491cd270
Maximiliano Ivan Biondo <mbiondo@hcdn.gob.ar>
2024-09-06 19:21:22 -0300

    chore: Update astro.config.mjs and package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import robotsTxt from 'astro-robots-txt';
-import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import robotsTxt from "astro-robots-txt"
+import vercel from "@astrojs/vercel/serverless"
 
-import sitemap from "astro-sitemap";
+import sitemap from "astro-sitemap"
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://lumalux.com.ar',
-  integrations: [tailwind(), robotsTxt(), sitemap()],
-  output: 'server',
-  adapter: vercel()
-});
+	site: "https://lumalux.com.ar",
+	integrations: [tailwind(), robotsTxt(), sitemap()],
+	output: "server",
+	adapter: vercel({
+		webAnalytics: { enabled: true, provider: "plausible", domain: "lumalux.com.ar" },
+	}),
+})
